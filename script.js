f8ba2f14056edf4ed8b7bba85001121e8be434b6
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2020-06-03 15:02:37 +0200

    Stipjes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import draw1 from "./works/work1.js";
 import draw2 from "./works/work2.js";
+import draw3 from "./works/work3.js";
 
 const canvas1 = document.getElementById("canvas1");
 const ctx1 = canvas1.getContext("2d");
@@ -8,3 +9,7 @@ draw1(ctx1);
 const canvas2 = document.getElementById("canvas2");
 const ctx2 = canvas2.getContext("2d");
 draw2(ctx2);
+
+const canvas3 = document.getElementById("canvas3");
+const ctx3 = canvas3.getContext("2d");
+draw3(ctx3);
