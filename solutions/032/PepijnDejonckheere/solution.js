98fa1f18344bc3a03d84f57a70d0e0901bd9c115
SimonVanherweghe <simon.vanherweghe@gmail.com>
2020-06-09 09:06:28 +0000

    Deploying to gh-pages from  @ 2b2b434f3d9372517039cc0068c112c2f59ccd82 🚀

---
+++ solutions/032/PepijnDejonckheere/solution.js
@@ -0,0 +1,24 @@
+const draw = function (ctx) {
+  const colors = [
+    "#4fc3f7",
+    "#0d47a1",
+    "#2196f3",
+    "#4fc3f7",
+  ];
+
+    ctx.fillStyle = "#b71c1c";
+    ctx.fillRect(15, 40, 20, 20);
+    
+    ctx.fillStyle = "#b71c1c";
+    ctx.fillRect(40, 40, 20, 20);
+    
+    ctx.fillStyle = "#b71c1c";
+    ctx.fillRect(65, 40, 20, 20);
+
+    ctx.fillStyle = "#2196f3";
+    ctx.fillRect(10, 50, 80, 2);
+
+  return ctx;
+};
+
+export default draw;
