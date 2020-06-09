4da7c6589595f591e390493cf695564046c69a6f
Milan Vanalderwerelt <milan.vanalderwerelt@student.howest.be>
2020-06-09 09:51:10 +0200

    issue #23 solution 015

---
+++ solutions/015/MilanVanalderwerel/solution.js
@@ -0,0 +1,11 @@
+const draw = function (ctx) {
+
+  ctx.beginPath();
+  ctx.rect(20, 20, 100, 100);
+  ctx.strokeStyle = "#b71c1c";
+  ctx.stroke();
+  return ctx;
+};
+
+export default draw;
+//015 #23
