c812915cbafb828c26f9408a61c6fbe15baebde8
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2020-06-09 11:26:56 +0200

    #35 quickfix om afbeelding te kunnen tonen

--- solutions/026/RobinVandenBerghe/solution.js
+++ solutions/026/RobinVandenBerghe/solution.js
@@ -4,8 +4,8 @@ const draw = function (ctx) {
   ctx.beginPath();
   ctx.strokeStyle = "#4fc3f7";
   ctx.fillStyle = "#4fc3f7";
-  ctx.arc(100, 150, 50, 0, 2 * Math.PI);
-  ctx.arc(200, 150, 50, 0, 2 * Math.PI);
+  ctx.arc(26, 50, 24, 0, 2 * Math.PI);
+  ctx.arc(74, 50, 24, 0, 2 * Math.PI);
   ctx.closePath();
   ctx.stroke();
   ctx.fill();
