4019ed4f1dbe329706e9a671d89e315dd908c10c
ilyeshammoumi <ilyes.hammoumi@gmail.com>
2020-06-09 10:49:00 +0200

    #45 solution 036

--- solutions/template/solution.js
+++ solutions/template/solution.js
@@ -1,5 +1,24 @@
+function cirkel(ctx, x, y,  height,  color) {
+
+  ctx.beginPath()
+  ctx.fillStyle = color;
+  //ctx.fillRect(x, y, width, height)
+  ctx.moveTo(x, y);
+  
+  ctx.arc(x, y, height, 0, 2 * Math.PI);   
+    
+  ctx.fill();
+
+
+}
 const draw = function (ctx) {
+  cirkel(ctx, 40, 10, 8, "#0d47a1");
+  cirkel(ctx, 40, 30, 8,  "#ef6c00");
+  cirkel(ctx, 40, 50, 8,  "#b71c1c");
+  cirkel(ctx, 40, 70, 8, "#2196f3");
+ 
   return ctx;
+  
 };
 
 export default draw;
